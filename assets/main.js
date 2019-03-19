86f8923c91ccc93e8fd707f896a3b3130bc8dfd7
Albert Suarez <alsumo95@gmail.com>
2019-03-19 05:38:34 +0000

    Show all texts with animation

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,18 @@
-var quotes = [
+let idx = 0;
+const quotes = [
     "nos vamos a comer",
-    "un arroz con bacala'o",
+    "un arroz con bacalao'",
     "alla en lo alto del puerto",
     "que mañana es domingo",
     "cuchibili cuchibili"
-]
+];
 
-function getCaption() {
-    var quote = quotes[Math.floor(Math.random() * quotes.length)];
-    return quote;
-}
+const update = () => {
+    document.getElementById("sarandonga").textContent = quotes[idx];
+    idx = (idx + 1) % quotes.length;
+    window.setTimeout(update, 3000);
+};
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    document.getElementById("sarandonga").innerHTML = getCaption();
+    update();
 })
